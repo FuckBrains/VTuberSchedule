f5228c084c272d08b88566957c367316f68f5a05
whistler228 <dpava111@yahoo.co.jp>
2020-11-02 03:19:52 +0900

    add clean script

--- notify/tasks.py
+++ notify/tasks.py
@@ -1,6 +1,6 @@
-from huey.contrib.djhuey import periodic_task, task
+from huey.contrib.djhuey import periodic_task, task, db_periodic_task
 from huey import crontab
-from django.shortcuts import render
+from django.utils import timezone
 from .models import NoticeScheduleStream, NoticeScheduleChannel
 
 from .views import check_condition, send_mail, send_web_push
@@ -34,3 +34,15 @@ def task_send_webpush(username, video_id_list):
     user = CustomUser.objects.get(username=username)
     for v in video_id_list:
         send_web_push(user, v)
+
+
+@db_periodic_task(crontab(day="*/7"))
+def task_clean_db_notice_stream():
+    logger.info("[Clean] Start cleaning NoticeScheduleStream...")
+    print("[Clean] Start cleaning NoticeScheduleStream...")
+    i = 0
+    for item in NoticeScheduleStream.objects.all():
+        if item.stream.start_at < timezone.now():
+            item.delete()
+            i += 1
+    logger.info(f"[Clean] Complete cleaning. {i} records deleted")
